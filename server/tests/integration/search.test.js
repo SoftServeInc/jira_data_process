8b279503a46a77cc088de9fc2a6f83658416c553
vvyshyvan <v-vvyshyvan@vrbo.com>
2022-08-16 12:44:16 -0500

    fix integration tests (to close server)

--- server/tests/integration/search.test.js
+++ server/tests/integration/search.test.js
@@ -1,5 +1,5 @@
 const app = require('../../server');
-const request = require('supertest');
+const agent = require('supertest');
 const {searchResponseStub} = require('./stubs');
 
 jest.mock('jira-connector', () => {
@@ -19,12 +19,15 @@ jest.mock('jira-connector', () => {
 });
 
 describe('Search integration tests', function () {
-    it('should return expected result', async () => {
-        const response = await request(app).get(
+    it('should return expected result', async (done) => {
+        const server = app.listen(done);
+        const request = agent(server);
+        const response = await request.get(
             '/api/search?jql=reporter%20=%20user%20AND%20project%20=%20project&jiraUrl=jira.com&username=user&password=password'
         );
 
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual(searchResponseStub);
+        server.close(done);
     });
 });
